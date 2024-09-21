4a366211e5131a1ca9add01708d7a6f817d404b4
Hasan Hasanzade <101423091+hasan-hasanzade@users.noreply.github.com>
2024-09-21 10:01:07 +0400

    modal changed

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,12 @@ document.addEventListener('DOMContentLoaded', function() {
     const modal = document.getElementById('modal');
     const downloadButton = document.getElementById('downloadButton');
 
-    // Hide the modal by default
     modal.style.display = 'none';
 
-    // Show modal on button click
     downloadButton.addEventListener('click', function() {
         modal.style.display = 'flex';
     });
 
-    // Close modal if user clicks outside of it
     window.addEventListener('click', function(event) {
         if (event.target === modal) {
             modal.style.display = 'none';

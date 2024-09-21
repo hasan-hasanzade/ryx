13b45fdbf38a9aa4c5c8f2f02b20c5e9736714c0
Hasan Hasanzade <101423091+hasan-hasanzade@users.noreply.github.com>
2024-09-21 09:48:40 +0400

    refactored

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,11 @@
 document.getElementById('downloadButton').addEventListener('click', function() {
-    document.getElementById('modal').style.display = 'block';
-  });
-  
-  // Close modal if user clicks outside of it
-  window.onclick = function(event) {
+    document.getElementById('modal').style.display = 'flex'; // Flex to center it
+});
+
+// Close modal if user clicks outside of it
+window.onclick = function(event) {
     const modal = document.getElementById('modal');
     if (event.target === modal) {
         modal.style.display = 'none';
     }
-  }
-  
+}

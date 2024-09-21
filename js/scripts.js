bcc24b0cb8808d41aa6e391e3f036b98780912e6
Hasan Hasanzade <101423091+hasan-hasanzade@users.noreply.github.com>
2024-09-21 09:51:13 +0400

    last refactored peace

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
-document.getElementById('downloadButton').addEventListener('click', function() {
-    document.getElementById('modal').style.display = 'flex'; // Flex to center it
-});
-
-// Close modal if user clicks outside of it
-window.onclick = function(event) {
+document.addEventListener('DOMContentLoaded', function() {
     const modal = document.getElementById('modal');
-    if (event.target === modal) {
-        modal.style.display = 'none';
-    }
-}
+    const downloadButton = document.getElementById('downloadButton');
+
+    // Hide the modal by default
+    modal.style.display = 'none';
+
+    // Show modal on button click
+    downloadButton.addEventListener('click', function() {
+        modal.style.display = 'flex';
+    });
+
+    // Close modal if user clicks outside of it
+    window.addEventListener('click', function(event) {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+});
